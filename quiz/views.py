67a3c0b259c40b4192a14c31a12b786e86d9ea03
KarlbomTjejerna <Malin.karlbom@svenskaspel.se>
2017-09-28 15:05:56 +0200

    skicka rätt svar

--- quiz/views.py
+++ quiz/views.py
@@ -37,12 +37,23 @@ def question(request, quiz_number, question_number):
 
 
 def completed(request, quiz_number):
+	quiz = Quiz.objects.get(quiz_number=quiz_number)
+    questions = list(quiz.questions.all())
+    saved_answers = request.session.get(str(quiz_number), {})
+    
+    num_questions = quiz.questions.count()
     context = {
-            "correct": 12,
-            "total": 20,
-        "quiz_number": quiz_number,
-    }
+        "correct": num_correct_answers,
+        "total": num_questions,
+}
+
     return render(request, "results.html", context)
+    num_correct_answers = 0
+    for question_number, answer in saved_answers.items():
+       correct_answer = questions[int(question_number) - 1].correct
+       if correct_answer == answer:
+        num_correct_answers = num_correct_answers + 1
+
 
 def answer(request, quiz_number, question_number):
     answer = request.POST["answer"]
